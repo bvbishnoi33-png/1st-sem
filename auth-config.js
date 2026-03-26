36f2f694b3adafd5c0da747977ea6d7603a2075b
bvbishnoi33-png <bvbishnoi33@gmail.com>
2026-03-26 09:31:04 +0530

    Create auth-config.js

---
+++ auth-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+
+apiKey: "AIzaSyAzLq1dCk0j2ph02-jSKoI1iD40WicGq7w",
+
+authDomain: "result-d8f94.firebaseapp.com",
+
+projectId: "result-d8f94",
+
+storageBucket: "result-d8f94.firebasestorage.app",
+
+messagingSenderId: "801990163775",
+
+appId: "1:801990163775:web:bf8aa105886af5f4e1678a"
+
+};
